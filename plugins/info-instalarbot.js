9159cf2184de9ad409d306511d4b17b24599313a
JhonatanPpp <119618637+JhonatanPpp@users.noreply.github.com>
2023-05-08 14:38:20 -0500

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,22 +1,18 @@
 import { generateWAMessageFromContent } from '@adiwajshing/baileys'
 let handler  = async (m, { conn }) => {
 let texto = `
-*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙱𝙾𝚇𝙼𝙸𝙽𝙴 𝙷𝙾𝚂𝚃*
-> Tutorial: https://youtu.be/eC9TfKICpcY
-> Pagina Oficial: https://boxmineworld.com
-> Dashboard: https://dash.boxmineworld.com/home
-> Panel: https://panel.boxmineworld.com
-> Soporte: https://discord.gg/84qsr4v 
+*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻*
+> Tutorial: https://www.youtube.com/watch?v=uWfbR_juSdY&ab_channel=Noka924
+> Pagina Oficial: https://www.youtube.com/watch?v=uWfbR_juSdY&ab_channel=Noka924
+> Dashboard: https://www.youtube.com/watch?v=uWfbR_juSdY&ab_channel=Noka924
+> Panel: https://www.youtube.com/watch?v=uWfbR_juSdY&ab_channel=Noka924
+> Soporte: https://www.youtube.com/watch?v=uWfbR_juSdY&ab_channel=Noka924
 
 ------------------------------------
 
-*—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂 𝚃𝙴𝚁𝙼𝚄𝚇*
-> Tutorial: https://youtu.be/yRS4m36Zwkw
+*—◉ 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂*
+> Tutorial: https://www.youtube.com/watch?v=uWfbR_juSdY&ab_channel=Noka924
 > Comandos:
-- cd && termux-setup-storage
-- apt-get update -y && apt-get upgrade -y
-- pkg install -y git nodejs ffmpeg imagemagick && pkg install yarn 
-- git clone https://github.com/BrunoSobrino/Mystic-termux.git && cd Mystic-termux 
 - npm start` 
 let aa = { quoted: m, userJid: conn.user.jid }
 let prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: `${texto}`.trim(), contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD' }, mentionedJid: [m.sender] }}}, aa)
